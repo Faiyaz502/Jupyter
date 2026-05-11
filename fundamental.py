4f3f10900c51b79c8e81dd03387fffb976195df7
Akib Faiyaz Fahim <www.fahim.faizas@gmail.com>
2026-05-11 17:28:33 +0600

    python

--- fundamental.py
+++ fundamental.py
@@ -1,15 +1,49 @@
-list1 = ['a','b',15,14];
+# list1 = ['a','b',15,14];
 
-list2 = list(list1);
+# list2 = list(list1);
 
-del(list2[1]) ;
+# del(list2[1]) ;
 
 
-a = [99 , "Bottle",['on','the','spot']];
+# a = [99 , "Bottle",['on','the','spot']];
 
-print(a[0:2]) ;
+# print(a[0:2]) ;
 
-print(a[1:]);
+# print(a[1:]);
+
+
+
+# If else statements
+
+x = 10
+
+if x > 5:
+    print("Greater")
+else:
+    print("Smaller")
+
+
+#Loop 
+
+#For
+for i in range(5):
+    print(i)
+
+#While 
+
+i=0 
+while i < 5 :
+    print("ith -",i)
+    i +=1 
+
+#Function
+
+def add(a,b):
+    return a+b
+
+result = add(5,5)
+
+print(result)
 
 
 
